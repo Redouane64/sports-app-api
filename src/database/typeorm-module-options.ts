78c541bc8aaab534efcbf97fd8533ff04351c77a
redouane <redouane.sobaihi@live.com>
2025-11-05 00:21:53 +0100

    fix: typeorm cli and datasource config

--- src/database/typeorm-module-options.ts
+++ src/database/typeorm-module-options.ts
@@ -4,16 +4,17 @@ import {
   TypeOrmModuleOptions,
 } from '@nestjs/typeorm';
 import { DatabaseConfig } from '../config/sections/database.config';
+import { User } from 'src/user/entities/user.entity';
 
 export const typeormModuleOptions: TypeOrmModuleAsyncOptions = {
   inject: [ConfigService],
   useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
     const databaseConfig = configService.get<DatabaseConfig>('database')!;
     return {
-      applicationName: 'TopPass.Backend',
+      applicationName: 'sports-app-api',
       type: 'postgres',
       url: databaseConfig.url,
-      entities: [],
+      entities: [User],
       synchronize: false,
       logger: 'advanced-console',
       logging: 'all',
