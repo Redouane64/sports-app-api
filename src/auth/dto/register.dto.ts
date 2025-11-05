7835eb0feb19698c4879d99db96d6dfe9d487c3f
redouane <redouane.sobaihi@live.com>
2025-11-05 21:21:46 +0100

    fix: add validation decorators

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
 export class RegisterUserParams {
+  @IsString()
+  @MinLength(2)
   fullName!: string;
+
+  @IsEmail()
   email!: string;
+
+  @IsString()
+  @MinLength(5)
   password!: string;
 }
