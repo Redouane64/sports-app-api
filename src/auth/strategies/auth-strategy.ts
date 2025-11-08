c98116c99ce2e0e6c056c9bcbe9ff57fb7f36c0e
redouane <redouane.sobaihi@live.com>
2025-11-08 13:37:39 +0100

    feat: query user database and session when authenticating users

--- src/auth/strategies/auth-strategy.ts
+++ src/auth/strategies/auth-strategy.ts
@@ -5,12 +5,17 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { AuthService } from '../auth.service';
 import { AuthenticatedUser, PurposeIds, TokenPayloadOf } from '../interfaces';
 import { ConfigProps } from '../../config';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class JwtAuthStrategy extends PassportStrategy(Strategy) {
   constructor(
     configService: ConfigService,
     private readonly authService: AuthService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {
     const authConfig = configService.get<ConfigProps['auth']>('auth')!;
     super({
@@ -20,18 +25,32 @@ export class JwtAuthStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(
+  async validate(
     payload: TokenPayloadOf<'authentication'>,
-  ): AuthenticatedUser | null {
+  ): Promise<AuthenticatedUser | null> {
     if (payload.purpose !== PurposeIds.authentication) {
       return null;
     }
 
-    // TODO: add user store look up logic
+    // TODO: Cache this
+    const entity = await this.userRepository.findOne({
+      where: {
+        id: payload.id,
+        sessions: {
+          id: payload.jti,
+        },
+      },
+      relationLoadStrategy: 'query',
+    });
+
+    if (!entity) {
+      return null;
+    }
+
     const user: AuthenticatedUser = {
-      id: payload.id,
-      email: payload.email,
-      fullName: payload.fullName,
+      id: entity.id,
+      email: entity.email,
+      fullName: entity.fullName,
       sessionId: payload.jti,
     };
 
