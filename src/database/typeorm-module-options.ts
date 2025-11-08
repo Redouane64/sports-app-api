e959c3bc4d2603fcebef31baaa71e12aa9e8082e
redouane <redouane.sobaihi@live.com>
2025-11-08 18:17:18 +0100

    feat: disable typeorm logger and enable synchronize in test environment

--- src/database/typeorm-module-options.ts
+++ src/database/typeorm-module-options.ts
@@ -8,12 +8,15 @@ import { User } from 'src/auth/entities/user.entity';
 import { Session } from 'src/auth/entities/session.entity';
 import { Track } from 'src/track/entities/track.entity';
 import { Record } from 'src/records/entities/record.entity';
+import { AppConfig } from 'src/config/sections/app.config';
 
 export const typeormModuleOptions: TypeOrmModuleAsyncOptions = {
   inject: [ConfigService],
   useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
     const databaseConfig = configService.get<DatabaseConfig>('database')!;
-    return {
+    const appConfig = configService.get<AppConfig>('app')!;
+
+    const moduleOptions: TypeOrmModuleOptions = {
       applicationName: 'sports-app-api',
       type: 'postgres',
       url: databaseConfig.url,
@@ -22,5 +25,15 @@ export const typeormModuleOptions: TypeOrmModuleAsyncOptions = {
       logger: 'formatted-console',
       logging: 'all',
     };
+
+    if (appConfig.nodeEnv === 'test') {
+      Object.assign(moduleOptions, {
+        logging: false,
+        logger: undefined,
+        synchronize: true,
+      });
+    }
+
+    return moduleOptions;
   },
 };
